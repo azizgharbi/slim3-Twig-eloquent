3eb87278b7b52dfe234b2a4c0b51b34fccf8da36
aziz <a.gharbi@emobiletechnologies.com>
2016-10-29 20:52:06 +0100

    seconde commit

--- public/index.php
+++ public/index.php
@@ -4,13 +4,11 @@ require 'config.php';
 
 $app->get('/', function ($request, $response) {
   $user=$this->db->table('users')->find(1);
-  var_dump($user);
-});
 
-// testing twig render 
-$app->get('/test', function ($request, $response, $args) {
-  return $this->view->render($response, 'home.twig', [
-          'name' => "mohsen"
-      ]);})->setName('home');
+  $this->view->render($response, 'home.twig', [
+              'user' => $user
+          ]);
+
+        });
 
 $app->run();

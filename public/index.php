4d1075c213e476370dec04c0418974fef055d1a2
aziz <a.gharbi@emobiletechnologies.com>
2016-10-29 21:16:34 +0100

    layouts

--- public/index.php
+++ public/index.php
@@ -4,11 +4,10 @@ require 'config.php';
 
 $app->get('/', function ($request, $response) {
   $user=$this->db->table('users')->find(1);
-
   $this->view->render($response, 'home.twig', [
               'user' => $user
           ]);
 
         });
 
-$app->run();
+$app->run(); 

dbb0f1809a450df0f341fb85ea3c4b4a7caf2aa2
aziz <a.gharbi@emobiletechnologies.com>
2016-10-30 21:27:50 +0100

    bootsrap

--- public/index.php
+++ public/index.php
@@ -1,24 +1,17 @@
 <?php
 
 require 'config.php';
+
 Use App\Models\User;
 
 $app->get('/', function ($request, $response) {
-  $user=User::find(1);
+  $user= User::find(1);
   $this->view->render($response, 'home.twig', [
               'user' => $user
           ]);
 
         });
 
-$app->get('/test', function ($request, $response) {
-$user = User::find(1);
-$user->name="Sami";
-$user->save();
-$this->view->render($response, 'test.twig', [
-            'user' => $user
-        ]);
-});
 
 $app->run();
 //https://akrabat.com/using-testing-eloquent-in-slim-framework/

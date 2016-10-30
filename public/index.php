d720d197c45fcb7f477a4b173b3a257c2d3fe949
aziz <a.gharbi@emobiletechnologies.com>
2016-10-30 01:09:17 +0100

    fixed

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require 'config.php';
 Use App\Models\User;
 
 $app->get('/', function ($request, $response) {
-  $user=$this->db->table('users')->find(1);
+  $user=User::find(1);
   $this->view->render($response, 'home.twig', [
               'user' => $user
           ]);

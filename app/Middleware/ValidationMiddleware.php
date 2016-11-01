fe31ff40b8af586bd8fb3a0a1bbb0fcd90e581a8
aziz <a.gharbi@emobiletechnologies.com>
2016-11-01 16:51:37 +0100

    validations

--- app/Middleware/ValidationMiddleware.php
+++ app/Middleware/ValidationMiddleware.php
@@ -9,8 +9,10 @@ class ValidationMiddleware extends Middleware
      {
 
         $this->container->view->getEnvironment()->addGlobal("errors",$_SESSION['errors']);
+        unset($_SESSION['errors']);
         $response = $next($request, $response);
         return $response;
+
 
            }
 
